02a5722e2c265593448091aaca6e2725d6fe2356
Daniel Pelissari <dpelissari1@gmail.com>
2026-06-27 18:46:23 -0300

    feat: enpoints de put e delete para medicamento e fabricante

---
+++ EndPoints/FabricanteEndpoints.cs
@@ -0,0 +1,75 @@
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedMinimalApi.Endpoints;
+
+public static class FabricanteEndpoints
+{
+    public static void MapFabricanteEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/fabricantes"); // Agrupa todas as rotas sob /fabricantes
+
+        group.MapGet("/", async (MedicamentoDb db) =>
+            await db.Fabricantes.ToListAsync()
+        );
+
+        group.MapGet("/{id:guid}", async (Guid id, MedicamentoDb db) => {
+            var fabricanteFromDb = await db.Fabricantes.Include(f => f.Medicamentos).FirstOrDefaultAsync(f => f.Id == id);
+            if (fabricanteFromDb is null)
+                return Results.NotFound($"Fabricante com o id {id} não encontrado");
+            return Results.Ok(fabricanteFromDb);
+        });
+
+        group.MapPost("/", async (Fabricante fabricante, MedicamentoDb db, IValidator<Fabricante> validator) =>
+        {
+            var validationResult = await validator.ValidateAsync(fabricante);
+            if (!validationResult.IsValid) 
+                return Results.ValidationProblem(validationResult.ToDictionary());
+
+            var cnpjExistente = await db.Fabricantes.AnyAsync(f => f.Cnpj == fabricante.Cnpj);
+            if (cnpjExistente)
+                return Results.BadRequest("Ja existe um fabricante cadastrado com o CNPJ informado");
+
+            fabricante.Id = Guid.NewGuid();
+            db.Fabricantes.Add(fabricante);
+            await db.SaveChangesAsync();
+            return Results.Created($"/{fabricante.Id}", fabricante);
+        });
+
+        group.MapPut("/{id:guid}", async (Guid id, Fabricante fabricante, MedicamentoDb db, IValidator<Fabricante> validator) =>
+        {
+            var validationResult = await validator.ValidateAsync(fabricante);
+            if (!validationResult.IsValid) 
+                return Results.ValidationProblem(validationResult.ToDictionary());
+
+            var fabricanteRegistro = await db.Fabricantes.FirstOrDefaultAsync(f => f.Id == id);
+            if (fabricanteRegistro is null)
+                return Results.NotFound($"Fabricante com o id {id} não encontrado");
+
+            var cnpjEmUso = await db.Fabricantes.AnyAsync(f => f.Cnpj == fabricante.Cnpj && f.Id != id);
+            if (cnpjEmUso)
+                return Results.BadRequest("CNPJ informado pertence a outro fabricante");
+
+            db.Entry(fabricanteRegistro!).CurrentValues.SetValues(fabricante);
+            fabricanteRegistro!.Id = id; 
+
+            await db.SaveChangesAsync();
+            return Results.Ok(fabricanteRegistro);
+        });
+
+        group.MapDelete("/{id:guid}", async (Guid id, MedicamentoDb db) =>
+        {
+            var fabricanteFromDb = await db.Fabricantes.FirstOrDefaultAsync(f => f.Id == id);
+            if (fabricanteFromDb is null)
+                return Results.NotFound("Fabricante não encontrado.");
+
+            var possuiMedicamentos = await db.Medicamentos.AnyAsync(m => m.FabricanteId == id);
+            if (possuiMedicamentos)
+                return Results.BadRequest("Não é possível excluir este fabricante porque ele possui medicamentos vinculados a ele.");    
+
+            db.Fabricantes.Remove(fabricanteFromDb);
+            await db.SaveChangesAsync();
+            return Results.NoContent();
+        });
+    }
+}
---
+++ EndPoints/MedicamentoEndpoints.cs
@@ -0,0 +1,61 @@
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedMinimalApi.Endpoints;
+
+public static class MedicamentoEndpoints
+{
+    public static void MapMedicamentoEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/medicamentos"); // Agrupa todas as rotas sob /medicamentos
+
+        group.MapGet("/", async (MedicamentoDb db) =>
+            await db.Medicamentos.ToListAsync()
+        );
+
+        group.MapGet("/{id:guid}", async (Guid id, MedicamentoDb db) => {
+            var medicamento = await db.Medicamentos.FirstOrDefaultAsync(m => m.Id == id);
+            if (medicamento is null)
+                return Results.NotFound("Nenhum medicamento encontrado com o ID informado.");
+            return Results.Ok(medicamento);
+        });
+
+        group.MapPost("/", async (Medicamento medicamento, MedicamentoDb db, IValidator<Medicamento> validator) =>
+        {
+            var validationResult = await validator.ValidateAsync(medicamento);
+            if (!validationResult.IsValid) 
+                return Results.ValidationProblem(validationResult.ToDictionary());
+
+            var fabricante = await db.Fabricantes.FirstOrDefaultAsync(f => f.Id == medicamento.FabricanteId);
+            if (fabricante is null)
+                return Results.BadRequest("Fabricante não encontrado.");
+
+            medicamento.Id = Guid.NewGuid();
+            db.Medicamentos.Add(medicamento);
+            await db.SaveChangesAsync();
+
+            return Results.Created($"/{medicamento.Id}", medicamento);
+        });
+
+        group.MapPut("/{id:guid}", async (Guid id, Medicamento medicamento, MedicamentoDb db, IValidator<Medicamento> validator) =>
+        {
+            var validationResult = await validator.ValidateAsync(medicamento);
+            if (!validationResult.IsValid) 
+                return Results.ValidationProblem(validationResult.ToDictionary());
+
+            var medicamentoFromDb = await db.Medicamentos.FindAsync(id);
+            if (medicamentoFromDb is null) 
+                return Results.NotFound($"Nenhum medicamento encontrado com o id {id}");
+
+            var fabricanteExiste = await db.Fabricantes.AnyAsync(f => f.Id == medicamento.FabricanteId);
+            if (!fabricanteExiste)
+                return Results.BadRequest($"Fabricante com o id {medicamento.FabricanteId} não encontrado");
+
+            db.Entry(medicamentoFromDb!).CurrentValues.SetValues(medicamento);
+            medicamentoFromDb!.Id = id;
+
+            await db.SaveChangesAsync();
+            return Results.Ok(medicamentoFromDb);
+        });
+    }
+}
